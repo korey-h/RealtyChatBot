1b416e26e14a70a625eb0e7cc0b08406406fcbd1
Егор Уфаев <eufaev@yandex.ru>
2024-10-03 22:14:47 +0500

    add templates for new permissions

--- permissions.py
+++ permissions.py
@@ -1,3 +1,33 @@
 def has_redaction_permission(title_message, user_id):
     return title_message.user.tg_id == user_id or (
         title_message.user.is_admin or title_message.user.is_moderator)
+
+
+def is_redact_period_active(user):
+    """Проверка, не истек ли период, когда доступно редактирование
+    сообщений. Учитывает оганичения Telegram"""
+    # user - это объект из модуля models.py
+    return True
+
+
+def is_resend_while_redaction_active(user):
+    """Проверка, доступна ли отправка отредактированного объявления
+    как нового. Т.е. допускается такая отправка, если истек период редактирования,
+    определенный Telegram"""
+    # user - это объект из модуля models.py
+    return True
+
+
+def is_resend_active(user):
+    """Проверка, доступна ли отправка отредактированного объявления
+    как нового (или повторная отправка)."""
+    # user - это объект из модуля models.py
+    return True
+
+
+def is_redac_available_again(user):
+    """Проверка, прошло ли время временного запрета с момента предыдущего 
+    редактирования"""
+    # user - это объект из модуля models.py
+    return True
+
